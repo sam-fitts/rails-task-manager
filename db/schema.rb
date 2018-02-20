647bae5dcfc26751a80b6d1f250ea6eaec214791
Sam Fitts <sam.fitts@mac.com>
2018-02-20 10:56:08 +0900

    added completed column default false

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180220014557) do
+ActiveRecord::Schema.define(version: 20180220014934) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "completed", default: false
   end
 
 end
